6987147b90c62d1e160cdcb0d9a20d1395296fcd
Nguyen Truong Chung <nt.chung@gloops.com>
2015-09-04 17:53:25 +0700

    + playable gems board

--- match3war/src/puzzle/Gem.js
+++ match3war/src/puzzle/Gem.js
@@ -1,36 +1,137 @@
 import ui.View;
 import ui.ImageView;
 import animate;
+import math.geom.Point as Point;
 
 import src.common.utils as utils;
+import src.common.constants as constants;
 
-exports = Class(ui.ImageView, function(supr) {
+exports = Class(ui.View, function(supr) {
    
     this.init = function(opts)
-    { 
-        opts = merge(opts, {  
+    {         
+        supr(this, 'init', [opts]);                
+        
+        this._gemSprite = new ui.ImageView({  
+            superview: this,
             anchorX: GLOBAL.gemWidth * 0.5,
             anchorY: GLOBAL.gemHeight * 0.5,
             width: GLOBAL.gemWidth,
             height: GLOBAL.gemHeight,
-            image: "resources/images/blocks/icon_block_" + utils.zeroFill(2, opts.type, '0') + ".png",
-        });
-        
-        supr(this, 'init', [opts]);                
-        
-        this._row = opts.row;
-        this._col = opts.col;        
-        this.style.x = this._col * GLOBAL.gemWidth;
-        this.style.y = this._row * GLOBAL.gemHeight;
+        });                                  
     };  
+    
+    this.onObtain = function(opts)
+    {        
+        this._type = opts.type;
+        this._row = opts.row;
+        this._col = opts.col;    
+        
+        this._gemSprite.setImage(this.getImageUrl());
+        
+        var originalPosition = this.getOriginalPosition();        
+        this.style.x = originalPosition.x;
+        this.style.y = originalPosition.y - constants.BOARD_HEIGHT * 1.25;        
+        this.style.visible = true;
+        
+        this.gravityFall();
+    }
+    
+    this.gravityFall = function()
+    {
+        var originalPosition = this.getOriginalPosition();        
+        animate(this).clear().now({x: originalPosition.x, y: originalPosition.y + 5}, 300, animate.easeIn)
+            .then({x: originalPosition.x, y: originalPosition.y - 10}, 100, animate.easeOut)
+            .then({x: originalPosition.x, y: originalPosition.y}, 100, animate.easeIn);
+    }
+    
+    this.getImageUrl = function()
+    {
+        return "resources/images/blocks/icon_block_" + utils.zeroFill(2, this._type, '0') + ".png";
+    }
     
     this.animateSelection = function()
     {
-        animate(this).clear().now({r: Math.PI * 2, zIndex: 1000.0}, 1000, animate.linear).then({r: 0.0}, 0, animate.linear).then(this.animateSelection);
+        this.reposition();
+        
+        if (this._gemSprite)
+        {
+            animateGemSelection.call(this);            
+        }
     }
     
     this.animateUnselection = function()
+    {   
+        if (this._gemSprite)
+        {
+            animateGemUnselection.call(this);
+        }
+    }
+    
+    this.reposition = function()
+    {
+        this.style.x = this._col * GLOBAL.gemWidth;
+        this.style.y = this._row * GLOBAL.gemHeight;
+    }
+    
+    this.feintSwap = function(otherGem)
+    {
+        var originalPosition = this.getOriginalPosition();
+        var otherPosition = otherGem.getOriginalPosition();
+        
+        animate(this).clear()
+            .now({x: otherPosition.x, y: otherPosition.y}, 200, animate.linear)
+            .then({x: originalPosition.x, y: originalPosition.y}, 200, animate.linear);
+    }
+    
+    this.swap = function(otherGem)
     {        
-        animate(this).now({r: Math.PI * 2}, 300, animate.easeIn).then({r: 0.0}, 0, animate.linear).then({zIndex: 0.0});
+        var myPosition = this.getOriginalPosition();
+        var otherPosition = otherGem.getOriginalPosition();
+        
+        animate(this).clear()
+            .now({x: otherPosition.x, y: otherPosition.y}, 200, animate.linear);
+        
+        animate(otherGem).clear()
+            .now({x: myPosition.x, y: myPosition.y}, 200, animate.linear);
+        
+        var temp = this._row;
+        this._row = otherGem._row;
+        otherGem._row = temp;
+        
+        temp = this._col;
+        this._col = otherGem._col;
+        otherGem._col = temp;
+    }
+    
+    this.getOriginalPosition = function()
+    {
+        var x = this._col * GLOBAL.gemWidth;
+        var y = this._row * GLOBAL.gemHeight;
+        return new Point(x, y);
+    }
+    
+    this.isMatched = function(otherGem)
+    {
+        return otherGem != null && this._type == otherGem._type;
+    }
+    
+    // Empty
+    this.render = function(ctx)
+    {
+        // This is only a placeholder, the real sprites are children of this.
     }
 });
+
+function animateGemSelection()
+{
+    // Rotate the gem
+    animate(this._gemSprite).clear().now({r: Math.PI * 2, zIndex: 1000.0}, 1000, animate.linear).then({r: 0.0}, 0, animate.linear).then(animateGemSelection.bind(this));        
+}
+
+function animateGemUnselection()
+{
+    // Ending the rotation
+    animate(this._gemSprite).clear().now({r: Math.PI * 2}, 500 * (Math.PI * 2 - this.style.r) / (Math.PI * 2), animate.easeOut).then({r: 0.0}, 0, animate.linear).then({zIndex: 0.0});   
+}
+
--- match3war/src/puzzle/GemsBoard.js
+++ match3war/src/puzzle/GemsBoard.js
@@ -1,8 +1,15 @@
 import ui.View;
-import ui.GestureView as GestureView;
+import ui.ViewPool as ViewPool;
+import math.geom.Vec2D as Vec2D;
 
 import src.puzzle.Gem as Gem; 
 import src.common.constants as constants;
+
+BoardState = {
+    Idle: 0,
+    Collapse: 1,
+    Explode: 2,
+};
 
 exports = Class(ui.View, function(supr) {
     this.init = function(opts)
@@ -14,10 +21,29 @@ exports = Class(ui.View, function(supr) {
             cols: 10,
         });
         
-        supr(this, 'init', [opts]);                
-        
+                
+        supr(this, 'init', [opts]);    
+                
         GLOBAL.gemWidth = constants.BOARD_WIDTH / opts.rows;
         GLOBAL.gemHeight = constants.BOARD_HEIGHT / opts.cols;
+        
+        this._interractionLock = 500;
+        this._intentSwapFromGem = null;
+        this._intentSwapToGem = null;
+        
+        this._checkForMatchesTargets = null;
+        this._state = BoardState.Explode;
+        
+        this._gemsPool = new ViewPool({
+            ctor: Gem,
+            initCount: opts.rows * opts.cols,
+            initOpts: {
+                superview: this,
+                width: GLOBAL.gemWidth,
+                height: GLOBAL.gemHeight,
+                type: 1
+            }
+        });
         
         this._rows = opts.rows;
         this._cols = opts.cols;
@@ -27,48 +53,452 @@ exports = Class(ui.View, function(supr) {
             this._gems[row] = new Array(opts.cols);
             for (var col = 0; col < opts.cols; ++col)
             {
-                var rndType = Math.floor(Math.min(Math.random() * 5, 4)) + 1;
-                var gem = new Gem({
-                    superview: this,
-                    row: row,
-                    col: col,
-                    type: rndType,
-                });
-                                
-                this._gems[row][col] = gem;
+                this.fillGemAt(row, col);
             }
         }    
-        
+                
         this._selectedGem = null;
+        this._touchBeginPoint = null;
         
         this.on('InputStart', function (event, point) {
             // Find pointed gem
             var row = Math.floor(point.y / GLOBAL.gemHeight);
-            if (row >= 0 && row < this._rows)
-            {
-                var col = Math.floor(point.x / GLOBAL.gemWidth);
-                if (col >= 0 && col < this._cols)
-                {
-                    this._selectedGem = this._gems[row][col];
-                    this._selectedGem.animateSelection();
-                }
+            var col = Math.floor(point.x / GLOBAL.gemWidth);
+            this._selectedGem = this.getGemAt(row, col);            
+            if (this._selectedGem)
+            {                
+                this._selectedGem.animateSelection();
+                this._touchBeginPoint = point;
             }
         });
         
         this.on('InputMove', function (event, point) {
-            console.log("Moved: " + point.x + "," + point.y);
+            if (this._touchBeginPoint && this._selectedGem)
+            {
+                var delta = new Vec2D({
+                    x: point.x - this._touchBeginPoint.x,
+                    y: point.y - this._touchBeginPoint.y
+                });
+                
+                var mag = delta.getMagnitude();
+                if (mag > constants.TOUCH_MOVE_THRESHOLD_FOR_SWAP_GEM)
+                {
+                    var fromRow = this._selectedGem._row;
+                    var fromCol = this._selectedGem._col;
+                    
+                    if (Math.abs(delta.y) > Math.abs(delta.x))
+                    {
+                        var dy = delta.y < 0 ? -1 : 1;
+                        var toGem = this.getGemAt(fromRow + dy, fromCol);
+                        
+                        this._intentSwapFromGem = this._selectedGem;
+                        this._intentSwapToGem = toGem;                        
+                    }
+                    else                        
+                    {
+                        var dx = delta.x < 0 ? -1 : 1;
+                        var toGem = this.getGemAt(fromRow, fromCol + dx);
+                        
+                        this._intentSwapFromGem = this._selectedGem;
+                        this._intentSwapToGem = toGem;
+                    }                    
+                }
+            }
         });
         
         this.on('InputOut', function (event, point) {
-            if (this._selectedGem)
-            {
-                this._selectedGem.animateUnselection();
-            }
-            this._selectedGem = null;
+            this.unselectCurrentGem();
         });
     };     
     
+    this.unselectCurrentGem = function() {
+        if (this._selectedGem)
+        {
+            this._selectedGem.animateUnselection();
+        }
+        this._selectedGem = null;
+        this._touchBeginPoint = null;
+    }
+    
+    // Override the render and let the board has empty background
     this.render = function(ctx) {
-        // Let it empty.
+    };   
+    
+    this.swapGems = function(fromGem, toGem)
+    {
+        // Wrong move
+        if (fromGem == null || toGem == null)
+        {
+            return;
+        }
+        
+        // The swap is pointless?
+        if (!this.checkSwap(fromGem, toGem))
+        {
+            fromGem.feintSwap(toGem);
+            toGem.feintSwap(fromGem);
+        }
+        // Swap succeeds
+        else
+        {
+            this._gems[fromGem._row][fromGem._col] = toGem;
+            this._gems[toGem._row][toGem._col] = fromGem;
+            
+            fromGem.swap(toGem);            
+        }
+        
+        this._interractionLock = 300;
+        this.unselectCurrentGem();
+    };
+    
+    this.checkSwap = function(fromGem, toGem)
+    {
+        // Note - this function is not thread-safe
+        var row = toGem._row;
+        var col = toGem._col;
+        var res = false;
+        
+        // temporary swap
+        this._gems[fromGem._row][fromGem._col] = toGem;
+        
+        // Vertical check
+        var verticleCount = 1;
+        for (var i = row + 1; i < this._rows; ++i)
+        {
+            var gem = this.getGemAt(i, col);
+                      
+            if (fromGem.isMatched(gem))
+            {
+                ++verticleCount;                
+            }
+            else
+            {
+                break;
+            }
+        }
+        for (var i = row - 1; i >= 0; --i)
+        {
+            var gem = this.getGemAt(i, col);
+            
+            if (fromGem.isMatched(gem))
+            {
+                ++verticleCount;                
+            }
+            else
+            {
+                break;
+            }
+        }        
+        if (verticleCount >= constants.LEAST_MATCHES_REQUIRED)
+        {
+            res = true;
+        }
+        
+        // Horizontal check
+        var horizontalCount = 1;
+        for (var i = col + 1; i < this._cols; ++i)
+        {
+            var gem = this.getGemAt(row, i);
+            
+            if (fromGem.isMatched(gem))
+            {
+                ++horizontalCount;
+            }
+            else
+            {
+                break;                
+            }
+        }
+        for (var i = col - 1; i >= 0; --i)
+        {
+            var gem = this.getGemAt(row, i);
+            
+            if (fromGem.isMatched(gem))
+            {
+                ++horizontalCount;
+            }
+            else
+            {
+                break;                
+            }
+        }        
+        if (horizontalCount >= constants.LEAST_MATCHES_REQUIRED)
+        {
+            res = true;
+        }
+        
+         // undo the swap
+        this._gems[fromGem._row][fromGem._col] = fromGem;
+        return res;
+    }
+        
+    this.getGemAt = function(row, col)
+    {
+        if (row >= 0 && row < this._rows)
+        {
+            if (col >= 0 && col < this._cols)
+            {
+                return this._gems[row][col];
+            }
+        }
+        
+        return null;
+    };
+    
+    this.tick = function(dt)
+    {
+        if (this._interractionLock > 0)
+        {
+            this._interractionLock -= dt;
+        }
+        else
+        {
+            if (this._state == BoardState.Collapse)
+            {
+                var checkCount = 0;
+                for (var row = this._rows - 1; row >= 0; --row)
+                {
+                    for (var col = 0; col < this._cols; ++col)
+                    {
+                        var gem = this._gems[row][col];
+                        checkCount += this.collapseGem(gem);
+                    }
+                }
+                
+                for (var row = 0; row < this._rows; ++row)
+                {
+                    for (var col = 0; col < this._cols; ++col)
+                    {
+                        checkCount += this.fillGemAt(row, col);
+                    }
+                }
+                
+                if (checkCount > 0)
+                {                       
+                    this._checkForMatchesTargets = null;
+                    this._state = BoardState.Explode;     
+                    
+                    this._interractionLock = 300;
+                }                
+                else
+                {
+                    this._state = BoardState.Idle;     
+                }
+            }
+            else if (this._state == BoardState.Explode)
+            {
+                var checkCount = 0;
+                
+                if (this._checkForMatchesTargets)
+                {
+                    var view = this;
+                    this._checkForMatchesTargets.forEach(function(element, index, array) {
+                        checkCount += view.explodeMatches(element); 
+                    });
+                }
+                else
+                {
+                    for (var row = 0; row < this._rows; ++row)
+                    {
+                        for (var col = 0; col < this._cols; ++col)
+                        {
+                            var gem = this._gems[row][col];
+                            checkCount += this.explodeMatches(gem);                        
+                        }
+                    }
+                }
+                
+                this._checkForMatchesTargets = null;
+                
+                if (checkCount > 0)
+                {
+                    this._state = BoardState.Collapse;
+                }
+                else
+                {                    
+                    this._state = BoardState.Idle;                         
+                }
+                
+                this._interractionLock = 100;
+            }            
+            
+            var swapFromGem = this._intentSwapFromGem;
+            var swapToGem = this._intentSwapToGem;
+            this._intentSwapFromGem = null;
+            this._intentSwapToGem = null;                
+            
+            if (swapFromGem && swapToGem)
+            {
+                this.swapGems(swapFromGem, swapToGem);                
+                
+                this._checkForMatchesTargets = [swapFromGem, swapToGem];
+                this._state = BoardState.Explode;                
+            }
+        }
+    }
+    
+    this.removeGemAt = function(row, col)
+    {
+        var gem = this.getGemAt(row, col);
+        if (gem)
+        {
+            this._gems[row][col] = null;
+            this._gemsPool.releaseView(gem);
+        }
+    }
+    
+    this.removeGem = function(gem)
+    {
+        if (gem)
+        {
+            this._gems[gem._row][gem._col] = null;
+            this._gemsPool.releaseView(gem);
+        }
+    }
+    
+    this.fillGemAt = function(row, col)
+    {
+        if (this._gems[row][col] == null)
+        {
+            var rndType = Math.floor(Math.min(Math.random() * 5, 4)) + 1;
+            var gem = this._gemsPool.obtainView();
+            gem.onObtain({
+                row: row,
+                col: col,
+                type: rndType,
+            });            
+
+            this._gems[row][col] = gem;
+            
+            return 1;
+        }
+        
+        return 0;
+    }
+    
+    this.collapseGem = function(gem)
+    {
+        if (gem)
+        {
+            var row = gem._row;
+            var col = gem._col;
+            
+            var fallToRow = null;
+            for (var i = row + 1; i < this._rows; ++i)
+            {
+                var g = this.getGemAt(i, col);
+                if (!g)
+                {
+                    fallToRow = i;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            
+            if (fallToRow)
+            {
+                gem._row = fallToRow;
+                gem.gravityFall();
+                
+                this._gems[row][col] = null;
+                this._gems[fallToRow][col] = gem;
+                
+                return 1;
+            }
+        }
+        
+        return 0;
+    }
+    
+    this.explodeMatches = function(fromGem)
+    {
+        if (fromGem)
+        {
+            var exploded = false;
+            var row = fromGem._row;
+            var col = fromGem._col;
+
+            // Vertical check
+            var verticalMatches = [];
+            for (var i = row + 1; i < this._rows; ++i)
+            {
+                var gem = this.getGemAt(i, col);
+                if (!fromGem.isMatched(gem))
+                {
+                    break;
+                }
+                else
+                {
+                    verticalMatches.push(gem);
+                }
+            }
+            for (var i = row - 1; i >= 0; --i)
+            {
+                var gem = this.getGemAt(i, col);
+                if (!fromGem.isMatched(gem))
+                {
+                    break;
+                }
+                else
+                {
+                    verticalMatches.push(gem);
+                }
+            }        
+            if (verticalMatches.length >= constants.LEAST_MATCHES_REQUIRED - 1)
+            {
+                var view = this;
+                verticalMatches.forEach(function(element, index, array) {
+                    view.removeGem(element);
+                });
+                
+                exploded = true;
+            }
+
+            // Horizontal check
+            var horizontalMatches = [];
+            for (var i = col + 1; i < this._cols; ++i)
+            {
+                var gem = this.getGemAt(row, i);
+                if (!fromGem.isMatched(gem))
+                {
+                    break;
+                }
+                else
+                {
+                    horizontalMatches.push(gem);
+                }
+            }
+            for (var i = col - 1; i >= 0; --i)
+            {
+                var gem = this.getGemAt(row, i);
+                if (!fromGem.isMatched(gem))
+                {
+                    break;
+                }
+                else
+                {
+                    horizontalMatches.push(gem);
+                }
+            }        
+            if (horizontalMatches.length >= constants.LEAST_MATCHES_REQUIRED - 1)
+            {
+                var view = this;
+                horizontalMatches.forEach(function(element, index, array) {
+                    view.removeGem(element);
+                });
+                
+                exploded = true;
+            }
+
+            if (exploded)
+            {
+                this.removeGem(fromGem);
+                return verticalMatches.length + horizontalMatches.length;
+            }
+        }
+        
+        return 0;
     }
 });
